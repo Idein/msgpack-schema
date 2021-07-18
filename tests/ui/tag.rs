f4b849ec08d4e2cebba96f7863a481236d4c307c
Yuichi Nishiwaki <yuichi.nishiwaki@icloud.com>
2021-07-19 00:17:13 +0900

    simplify tests

--- tests/ui/tag.rs
+++ tests/ui/tag.rs
@@ -2,61 +2,22 @@ use msgpack_schema::*;
 
 #[derive(Serialize)]
 struct S1 {
-    #[tag = 0]
-    age: u32,
-    name: String,
+    x: String,
 }
 
 #[derive(Deserialize)]
 struct S2 {
-    #[tag = 0]
-    age: u32,
-    name: String,
-}
-
-#[derive(Serialize)]
-struct S3 {
-    #[tag = 0]
-    #[tag = 1]
-    name: String,
-}
-
-#[derive(Deserialize)]
-struct S4 {
-    #[tag = 0]
-    #[tag = 1]
-    age: u32,
-    name: String,
+    x: String,
 }
 
 #[derive(Serialize)]
 enum E1 {
-    #[tag = 0]
-    Cat,
-    Dog,
+    V,
 }
 
 #[derive(Deserialize)]
 enum E2 {
-    #[tag = 0]
-    Cat,
-    Dog,
-}
-
-#[derive(Serialize)]
-enum E3 {
-    #[tag = 0]
-    #[tag = 1]
-    Cat,
-    Dog,
-}
-
-#[derive(Deserialize)]
-enum E4 {
-    #[tag = 0]
-    #[tag = 1]
-    Cat,
-    Dog,
+    V,
 }
 
 fn main() {}
