f44ae04fe1901ac9f4e2a36b2655f3192ff67252
Yuichi Nishiwaki <yuichi.nishiwaki@icloud.com>
2023-03-25 21:14:09 +0900

    improve docs

--- src/lib.rs
+++ src/lib.rs
@@ -14,64 +14,83 @@
 //! }
 //! ```
 //!
+//! Compared with other schema languages like `rmp-serde`, `msgpack-schema` allows to specify more compact data representation, e.g., fixints as field keys, fixints as variant keys, etc.
+//!
 //! # Behaviours of serializers and deserializers
-//!
-//! ## Some general rules
-//!
-//! - The deserializer ignores irrelevant key-value pairs in MsgPack map objects.
-//! - MsgPack map objects must not have duplicate keys.
 //!
 //! ## Structs with named fields
 //!
-//! Structs with named fields will be serialized into a MsgPack map object whose keys are fixints specified by `#[tag]` attributes.
+//! Structs with named fields are serialized into a `Map` object where keys are fixints specified by `#[tag]` attributes.
+//! The current implementation serializes fields in order but one must not rely on this behavior.
 //!
-//! <table>
-//! <tr>
-//! <th>
-//! schema
-//! </th>
-//! <th>
-//! Rust
-//! </th>
-//! <th>
-//! MessagePack
-//! </th>
-//! </tr>
-//! <tr>
-//! <td>
+//! The deserializer interprets `Map` objects to create such structs.
+//! Field order is irrelevant to the result.
+//! If `Map` objects contains extra key-value pairs which are not contained in the definition of the struct, the deserializer simply ignores them.
+//! It is validation error when `Map` objects have two or more values with the same key.
 //!
-//! ```rust,ignore
+//! ```
+//! # use msgpack_schema::*;
+//! # #[derive(Debug, PartialEq, Eq)]
+//! #[derive(Serialize, Deserialize)]
 //! struct S {
 //!     #[tag = 0]
-//!     foo: u32,
+//!     x: u32,
 //!     #[tag = 1]
-//!     bar: String,
+//!     y: String,
 //! }
+//!
+//! let s = S {
+//!   x: 42,
+//!   y: "hello".to_owned(),
+//! };
+//!
+//! let b = b"\x82\x00\x2A\x01\xA5\x68\x65\x6c\x6c\x6f"; // 10 bytes; `{ 0: 42, 1: "hello" }`
+//! assert_eq!(serialize(&s), b);
+//! assert_eq!(s, deserialize(b).unwrap());
+//!
+//! // ignores irrelevant key-value pairs
+//! let b = b"\x83\x00\x2A\x02\xC3\x01\xA5\x68\x65\x6c\x6c\x6f"; // 12 bytes; `{ 0: 42, 2: true, 1: "hello" }`
+//! assert_eq!(s, deserialize(b).unwrap());
+//!
+//! // maps with duplicate keys are invalid input
+//! let b = b"\x83\x00\x2A\x00\xC3\x01\xA5\x68\x65\x6c\x6c\x6f"; // 12 bytes; `{ 0: 42, 0: true, 1: "hello" }`
+//! assert!(matches!(deserialize::<S>(b).unwrap_err(), DeserializeError::InvalidInput(_)));
 //! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```rust,ignore
-//! S { foo: 42, bar: "hello".to_owned() }
-//! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```js
-//! { 0: 42, 1: "hello" }
-//! ```
-//!
-//! </td>
-//! </tr>
-//! </table>
 //!
 //! Fields in named structs may be tagged with `#[optional]`.
 //!
 //! - The tagged field must be of type `Option<T>`.
 //! - On serialization, the key-value pair will not be included in the result map object when the field data contains `None`.
 //! - On deserialization, the field of the result struct will be filled with `None` when the given MsgPack map object contains no corresponding key-value pair.
+//!
+//! ```
+//! # use msgpack_schema::*;
+//! # #[derive(Debug, PartialEq, Eq)]
+//! #[derive(Serialize, Deserialize)]
+//! struct S {
+//!     #[tag = 0]
+//!     x: u32,
+//!     #[optional]
+//!     #[tag = 1]
+//!     y: Option<String>,
+//! }
+//!
+//! let s = S {
+//!   x: 42,
+//!   y: Some("hello".to_owned()),
+//! };
+//! let b = b"\x82\x00\x2A\x01\xA5\x68\x65\x6c\x6c\x6f"; // 10 bytes; `{ 0: 42, 1: "hello" }`
+//! assert_eq!(serialize(&s), b);
+//! assert_eq!(s, deserialize(b).unwrap());
+//!
+//! let s = S {
+//!   x: 42,
+//!   y: None,
+//! };
+//! let b = b"\x81\x00\x2A"; // 3 bytes; `{ 0: 42 }`
+//! assert_eq!(serialize(&s), b);
+//! assert_eq!(s, deserialize(b).unwrap());
+//! ```
 //!
 //! The `#[flatten]` attribute is used to factor out a single definition of named struct into multiple ones.
 //!
@@ -102,309 +121,129 @@
 //! assert_eq!(serialize(S2 { s1: S1 { x: 42 }, y: 43, }), serialize(S3 { x: 42, y: 43 }));
 //! ```
 //!
-//! ## Untagged structs with named fields
-//!
 //! Structs with named fields may be attached `#[untagged]`.
 //! Untagged structs are serialized into an array and will not contain tags.
 //!
-//! <table>
-//! <tr>
-//! <th>
-//! schema
-//! </th>
-//! <th>
-//! Rust
-//! </th>
-//! <th>
-//! MessagePack
-//! </th>
-//! </tr>
-//! <tr>
-//! <td>
-//!
-//! ```rust,ignore
+//! ```
+//! # use msgpack_schema::*;
+//! # #[derive(Debug, PartialEq, Eq)]
+//! #[derive(Serialize, Deserialize)]
 //! #[untagged]
 //! struct S {
-//!     foo: u32,
-//!     bar: String,
+//!     x: u32,
+//!     y: String,
 //! }
+//!
+//! let s = S {
+//!   x: 42,
+//!   y: "hello".to_owned(),
+//! };
+//! let b = b"\x92\x2A\xA5\x68\x65\x6c\x6c\x6f"; // 8 bytes; `[ 42, "hello" ]`
+//!
+//! assert_eq!(serialize(&s), b);
+//! assert_eq!(s, deserialize(b).unwrap());
 //! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```rust,ignore
-//! S { foo: 42, bar: "hello".to_owned() }
-//! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```js
-//! [ 42, "hello" ]
-//! ```
-//!
-//! </td>
-//! </tr>
-//! </table>
 //!
 //! ## Newtype structs
 //!
 //! Tuple structs with only one element are treated transparently.
 //!
-//! <table>
-//! <tr>
-//! <th>
-//! schema
-//! </th>
-//! <th>
-//! Rust
-//! </th>
-//! <th>
-//! MessagePack
-//! </th>
-//! </tr>
-//! <tr>
-//! <td>
-//!
-//! ```rust,ignore
-//! struct S(u32)
 //! ```
+//! # use msgpack_schema::*;
+//! # #[derive(Debug, PartialEq, Eq)]
+//! #[derive(Serialize, Deserialize)]
+//! struct S(u32);
 //!
-//! </td>
-//! <td>
+//! let s = S(42);
+//! let b = b"\x2A"; // 1 byte; `42`
 //!
-//! ```rust,ignore
-//! S(42)
+//! assert_eq!(serialize(&s), b);
+//! assert_eq!(s, deserialize(b).unwrap());
 //! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```js
-//! 42
-//! ```
-//!
-//! </td>
-//! </tr>
-//! </table>
 //!
 //! ## Unit structs and empty tuple structs
 //!
-//! Serialization and deserialization of unit structs and empty tuple structs are currently unsupported.
+//! Serialization and deserialization of unit structs and empty tuple structs are intentionally unsupported.
 //!
-//! <table>
-//! <tr>
-//! <th>
-//! schema
-//! </th>
-//! <th>
-//! Rust
-//! </th>
-//! <th>
-//! MessagePack
-//! </th>
-//! </tr>
-//! <tr>
-//! <td>
-//!
-//! ```rust,ignore
-//! struct S
 //! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```rust,ignore
-//! S
+//! // It is error to derive `Serialize` / `Deserialize` for these types of structs.
+//! struct S1;
+//! struct S2();
 //! ```
-//!
-//! </td>
-//! <td>
-//!
-//! UNSUPPORTED
-//!
-//! </td>
-//! </tr>
-//! <tr>
-//! <td>
-//!
-//! ```rust,ignore
-//! struct S()
-//! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```rust,ignore
-//! S()
-//! ```
-//!
-//! </td>
-//! <td>
-//!
-//! UNSUPPORTED
-//!
-//! </td>
-//! </tr>
-//! </table>
 //!
 //! ## Tuple structs
 //!
 //! Tuple structs with more than one element are encoded as an array.
 //! It is validation error to deserialize an array with unmatched length.
 //!
-//! <table>
-//! <tr>
-//! <th>
-//! schema
-//! </th>
-//! <th>
-//! Rust
-//! </th>
-//! <th>
-//! MessagePack
-//! </th>
-//! </tr>
-//! <tr>
-//! <td>
-//!
-//! ```rust,ignore
-//! struct S(u32, bool)
 //! ```
+//! # use msgpack_schema::*;
+//! # #[derive(Debug, PartialEq, Eq)]
+//! #[derive(Serialize, Deserialize)]
+//! struct S(u32, bool);
 //!
-//! </td>
-//! <td>
+//! let s = S(42, true);
+//! let b = b"\x92\x2A\xC3"; // 3 bytes; `[ 42, true ]`
 //!
-//! ```rust,ignore
-//! S(42, true)
+//! assert_eq!(serialize(&s), b);
+//! assert_eq!(s, deserialize(b).unwrap());
 //! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```js
-//! [ 42, true ]
-//! ```
-//!
-//! </td>
-//! </tr>
-//! </table>
 //!
 //! ## Unit variants and empty tuple variants
 //!
 //! Unit variants and empty tuple variants are serialized into a single fixint whose value is determined by the tag.
 //!
-//! <table>
-//! <tr>
-//! <th>
-//! schema
-//! </th>
-//! <th>
-//! Rust
-//! </th>
-//! <th>
-//! MessagePack
-//! </th>
-//! </tr>
-//! <tr>
-//! <td>
-//!
-//! ```rust,ignore
+//! ```
+//! # use msgpack_schema::*;
+//! # #[derive(Debug, PartialEq, Eq)]
+//! #[derive(Serialize, Deserialize)]
 //! enum E {
 //!     #[tag = 3]
 //!     Foo
 //! }
+//!
+//! let e = E::Foo;
+//! let b = b"\x03"; // 1 byte; `3`
+//!
+//! assert_eq!(serialize(&e), b);
+//! assert_eq!(e, deserialize(b).unwrap());
 //! ```
 //!
-//! </td>
-//! <td>
-//!
-//! ```rust,ignore
-//! E::Foo
 //! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```js
-//! 3
-//! ```
-//!
-//! </td>
-//! </tr>
-//!
-//! <tr>
-//! <td>
-//!
-//! ```rust,ignore
+//! # use msgpack_schema::*;
+//! # #[derive(Debug, PartialEq, Eq)]
+//! #[derive(Serialize, Deserialize)]
 //! enum E {
 //!     #[tag = 3]
 //!     Foo()
 //! }
+//!
+//! let e = E::Foo();
+//! let b = b"\x03"; // 1 byte; `3`
+//!
+//! assert_eq!(serialize(&e), b);
+//! assert_eq!(e, deserialize(b).unwrap());
 //! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```rust,ignore
-//! E::Foo()
-//! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```js
-//! 3
-//! ```
-//!
-//! </td>
-//! </tr>
-//! </table>
 //!
 //! ## Newtype variants
 //!
 //! Newtype variants (one-element tuple variants) are serialized into an array of the tag and the inner value.
 //!
-//! <table>
-//! <tr>
-//! <th>
-//! schema
-//! </th>
-//! <th>
-//! Rust
-//! </th>
-//! <th>
-//! MessagePack
-//! </th>
-//! </tr>
-//! <tr>
-//! <td>
-//!
-//! ```rust,ignore
+//! ```
+//! # use msgpack_schema::*;
+//! # #[derive(Debug, PartialEq, Eq)]
+//! #[derive(Serialize, Deserialize)]
 //! enum E {
 //!     #[tag = 3]
 //!     Foo(u32)
 //! }
+//!
+//! let e = E::Foo(42);
+//! let b = b"\x92\x03\x2A"; // 3 bytes; `[ 3, 42 ]`
+//!
+//! assert_eq!(serialize(&e), b);
+//! assert_eq!(e, deserialize(b).unwrap());
 //! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```rust,ignore
-//! E::Foo(42)
-//! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```js
-//! [ 3, 42 ]
-//! ```
-//!
-//! </td>
-//! </tr>
-//! </table>
 //!
 //! ## Untagged variants
 //!
@@ -412,47 +251,22 @@
 //! Serializing untagged variants results in the same data layout as the inner type.
 //! The deserializer deserializes into an untagged enum type by trying deserization one by one from the first variant to the last.
 //!
-//! <table>
-//! <tr>
-//! <th>
-//! schema
-//! </th>
-//! <th>
-//! Rust
-//! </th>
-//! <th>
-//! MessagePack
-//! </th>
-//! </tr>
-//! <tr>
-//! <td>
-//!
-//! ```rust,ignore
+//! ```
+//! # use msgpack_schema::*;
+//! # #[derive(Debug, PartialEq, Eq)]
 //! #[derive(Serialize, Deserialize)]
 //! #[untagged]
-//! enum Animal {
+//! enum E {
 //!     Foo(String),
 //!     Bar(u32),
 //! }
+//!
+//! let e = E::Bar(42);
+//! let b = b"\x2A"; // 1 byte; `42`
+//!
+//! assert_eq!(serialize(&e), b);
+//! assert_eq!(e, deserialize(b).unwrap());
 //! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```rust,ignore
-//! E::Bar(42)
-//! ```
-//!
-//! </td>
-//! <td>
-//!
-//! ```js
-//! 42
-//! ```
-//!
-//! </td>
-//! </tr>
-//! </table>
 //!
 
 use byteorder::BigEndian;
