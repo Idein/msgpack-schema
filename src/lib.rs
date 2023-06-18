f314de9d08a0f3b0d274d76ee4777c3b2c9107e5
Yuichi Nishiwaki <yuichi.nishiwaki@icloud.com>
2023-06-18 13:34:59 +0900

    typo

--- src/lib.rs
+++ src/lib.rs
@@ -1153,7 +1153,7 @@ pub fn serialize<S: Serialize>(s: S) -> Vec<u8> {
 
 /// Read out a MessagePack object.
 /// 
-/// If the input contains extra bytes following to a valid msgpack object,
+/// If the input contains extra bytes following a valid msgpack object,
 /// this function silently ignores them.
 pub fn deserialize<D: Deserialize>(r: &[u8]) -> Result<D, DeserializeError> {
     let mut deserializer = Deserializer::new(r);
